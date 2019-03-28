19a1d036481fa4c308180d53c70d5285234909d6
Vladimir Yuldashev <misterio92@gmail.com>
2019-03-28 23:57:01 +0300

    wip

--- java/src/main/java/com/example/demo/DemoApplication.java
+++ java/src/main/java/com/example/demo/DemoApplication.java
@@ -1,24 +1,15 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import javax.xml.stream.XMLInputFactory;
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
-
-@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException, FactoryConfigurationError {
@@ -26,7 +17,7 @@ public class DemoApplication {
 
         XMLInputFactory inputFactory = XMLInputFactory.newInstance();
 
-        //inputFactory.setProperty("javax.xml.stream.isCoalescing", True)
+        inputFactory.setProperty("javax.xml.stream.isCoalescing", true);
 
         // Setup a new eventReader
         InputStream in = new FileInputStream("AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML");
@@ -76,21 +67,17 @@ public class DemoApplication {
 
         File csvOutputFile = new File("ADDRESS_OBJECTS.csv");
 
-        PrintWriter pw = new PrintWriter(csvOutputFile);
+        PrintWriter writer = new PrintWriter(csvOutputFile);
 
         while (eventReader.hasNext()) {
             XMLEvent event = eventReader.nextEvent();
 
             HashMap<String, String> addressObject = new HashMap<>();
 
-            //reach the start of an item
             if (event.isStartElement()) {
-
                 StartElement startElement = event.asStartElement();
 
                 if (startElement.getName().getLocalPart().equals("Object")) {
-
-                    // attribute
                     Iterator<Attribute> attributes = startElement.getAttributes();
                     while (attributes.hasNext()) {
                         Attribute attribute = attributes.next();
@@ -102,50 +89,23 @@ public class DemoApplication {
                         }
                     }
                 }
-
-                // data
-                if (event.isStartElement()) {
-
-//                    if (event.asStartElement().getName().getLocalPart().equals("thetext")) {
-//                        event = eventReader.nextEvent();
-//
-//                        if (item.getFirstText() == null) {
-//                            System.out.println("thetext: "
-//                                    + event.asCharacters().getData());
-//                            item.setFirstText("notnull");
-//                            continue;
-//                        } else {
-//                            continue;
-//                        }
-//
-//                    }
-                }
             }
-
-            //reach the end of an item
-            if (event.isEndElement()) {
-                EndElement endElement = event.asEndElement();
-                if (endElement.getName().getLocalPart().equals("Object")) {
-//                    item = null;
-                }
-            }
-
             // write to csv
             String s = implode(";", addressObject);
 
-            pw.print(s);
+            writer.print(s);
         }
-
-        System.out.println("JAva finished");
     }
 
     private static String implode(String delimiter, Map<String, String> map) {
-
         boolean first = true;
         StringBuilder sb = new StringBuilder();
 
         for (Map.Entry<String, String> e : map.entrySet()) {
-            if (!first) sb.append(" " + delimiter + " ");
+            if (!first) {
+                sb.append(" " + delimiter + " ");
+            }
+
             sb.append(e.getValue());
             first = false;
         }
