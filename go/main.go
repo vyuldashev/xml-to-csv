4a9a19d37eca43f3f4ef99ae91361dbea695ef93
esazykin <evgeniy.sazykin@vrbank.ru>
2019-03-29 17:51:57 +0300

    plain process

--- go/main.go
+++ go/main.go
@@ -52,10 +52,13 @@ var columns = []string{
 
 func main() {
 	f, _ := os.Open("../AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML")
-	defer f.Close()
+	result, _ := os.Create("result.csv")
 
-	csvRawChannel := make(chan []string)
-	go write(csvRawChannel)
+	defer f.Close()
+	defer result.Close()
+
+	writer := csv.NewWriter(result)
+	defer writer.Flush()
 
 	r := bufio.NewReader(f)
 	d := xml.NewDecoder(r)
@@ -67,48 +70,32 @@ func main() {
 			break
 		}
 
-		go read(csvRawChannel, token)
+		switch se := token.(type) {
+		case xml.StartElement:
+			if se.Name.Local != "Object" {
+				continue
+			}
+
+			data := make(map[string]string, len(se.Attr))
+			for _, a := range se.Attr {
+				data[a.Name.Local] = a.Value
+			}
+
+			item := make([]string, len(columns))
+
+			for index, attr := range columns {
+				if v, ok := data[attr]; ok {
+					item[index] = v
+				} else {
+					item[index] = ""
+				}
+			}
+
+			writer.Write(item)
+		}
 	}
 
 	fmt.Println("Waiting for all writes...")
 
 	fmt.Println("Done")
-}
-
-func read(csvRawChannel chan []string, token xml.Token) {
-	switch se := token.(type) {
-	case xml.StartElement:
-		if se.Name.Local != "Object" {
-			return
-		}
-
-		data := make(map[string]string, len(se.Attr))
-		for _, a := range se.Attr {
-			data[a.Name.Local] = a.Value
-		}
-
-		item := make([]string, len(columns))
-
-		for index, attr := range columns {
-			if v, ok := data[attr]; ok {
-				item[index] = v
-			} else {
-				item[index] = ""
-			}
-		}
-
-		csvRawChannel <- item
-	}
-}
-
-func write(csvRawChannel chan []string) {
-	result, _ := os.Create("result.csv")
-	defer result.Close()
-
-	writer := csv.NewWriter(result)
-	defer writer.Flush()
-
-	for item := range csvRawChannel {
-		writer.Write(item)
-	}
 }
