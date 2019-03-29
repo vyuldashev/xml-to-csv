d998b06945341147df97300348494d222bd89517
Vladimir Yuldashev <misterio92@gmail.com>
2019-03-29 21:17:26 +0300

    docker for php

--- php/index.php
+++ php/index.php
@@ -68,7 +68,7 @@ $parser = xml_parser_create();
 
 xml_set_element_handler($parser, 'startElement', 'endElement');
 
-$f = fopen('../AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML', 'rb');
+$f = fopen('./files/AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML', 'rb');
 
 while ($data = fread($f, 32 * 1024)) {
     xml_parse($parser, $data);
