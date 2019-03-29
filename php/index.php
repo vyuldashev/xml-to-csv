fb8c52046191f860b716c0cdf4aabf6a41e1a99d
Vladimir Yuldashev <misterio92@gmail.com>
2019-03-29 19:32:02 +0300

    wip

--- php/index.php
+++ php/index.php
@@ -42,22 +42,38 @@ $columns = [
     'DIVTYPE',
 ];
 
-$xmlReader = new XMLReader();
-$xmlReader->open('../AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML', null, LIBXML_COMPACT | LIBXML_PARSEHUGE);
-
 $result = fopen('php.csv', 'wb');
 
-while ($xmlReader->read()) {
-    if ($xmlReader->name === 'Object') {
-        $addressObject = [];
+function startElement($parser, $name, $attrs)
+{
+    global $columns, $result;
+
+    if ($name === 'OBJECT') {
+        $addressObject = '';
 
         foreach ($columns as $column) {
-            $addressObject[$column] = $xmlReader->getAttribute($column);
+            $addressObject .= ($attrs[$column] ?? '') . ';';
         }
 
-        fputcsv($result, $addressObject);
+        fwrite($result, $addressObject . PHP_EOL);
     }
 }
 
+function endElement($parser, $name)
+{
+
+}
+
+$parser = xml_parser_create();
+
+xml_set_element_handler($parser, 'startElement', 'endElement');
+
+$f = fopen('../AS_ADDROBJ_20190324_a1a706ea-4ac7-43e7-b65b-68de81a57ddb.XML', 'rb');
+
+while ($data = fread($f, 32 * 1024)) {
+    xml_parse($parser, $data);
+}
+
+xml_parser_free($parser);
+fclose($f);
 fclose($result);
-$xmlReader->close();
